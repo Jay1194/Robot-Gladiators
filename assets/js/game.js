ef573fae7a19a4fa73b588abf0ef30af0b70c7ea
Jay <j.thomastt1@gmail.com>
2023-05-20 13:51:32 -0600

    fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -158,7 +158,7 @@ var endGame = function() {
   if (playerHealth > 0) {
     window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
   }
-  else {
+   else {
     window.alert("You've lost your robot in battle.");
   }
 
